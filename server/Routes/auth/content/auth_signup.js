fb2182936b5a6b4dfb85f4cd4e98d558138dfacc
FlynnHillier <flynnhillier@gmail.com>
2022-03-04 21:05:28 +0000

    configured auth/signup using request validation  + implemented authKey generation

--- server/Routes/auth/content/auth_signup.js
+++ server/Routes/auth/content/auth_signup.js
@@ -2,91 +2,139 @@ const path = require("path")
 const express = require("express")
 const bcrypt = require("bcrypt")
 const { checkSchema , validationResult } = require("express-validator")
-
+const randomstring = require("randomstring")
 
 function build_auth_signup_router(mongoose_instance,config){
+    
+    function create_user(email,password_plainText,authKey){
+        return new Promise((resolve,reject)=>{
+            config.mongo.mongoose_models.user.create({
+                email:email,
+                password:bcrypt.hashSync(password_plainText,config.mongo.user_creation.salt_rounds),
+                authKey:authKey
+            }).then((result)=>{
+                resolve()
+            }).catch((error)=>{
+                reject( {
+                    expected:false,
+                    message:"unable to create user"
+                })
+            })
 
+        })
+    }
+    
+    function gen_authKey(attempt_count=0){
+        return new Promise((resolve,rejects)=>{
+    
+            if(attempt_count >= 4){
+                throw {
+                    expected:false,
+                    message:`after ${attempt_count} attempts to gen a bespoke authKey, a bespoke authKey was still not generated.`
+                }
+            }
+    
+            let genned_key = randomstring.generate({
+                length:42,
+                charset:"alphabetic"
+            })
+    
+            config.mongo.mongoose_models.user.findOne({
+                authKey:genned_key
+            }).then((result)=>{
+                if(result === null){
+                    resolve(genned_key)
+                } else{
+                    gen_authKey(attempt_count += 1)
+                    .catch((err)=>{
+                        rejects(err)
+                    }).then((genned_key)=>{
+                        resolve(genned_key)
+                    })
+                }
+            })
+        })
+    }
+    
+    
+    
+    //#ROUTE
+    
     const auth_signup_router = express.Router()
 
-    
     auth_signup_router
     .route("/")
     .post(
-            checkSchema({
-            email:{
-                in:["body"],
-                trim:true,
-                isEmpty:{
-                    negated:true,
-                    errorMessage:"email field empty",
-                    bail:true,
-                },
-                isEmail:{
-                    errorMessage:"not a valid email",
-                    bail:true,
-                },
+        checkSchema({
+                email:config.req_schemas.components.email,
+                password:config.req_schemas.components.password            
+            }),
+        (req,res,next)=>{
 
-                errorMessage:"invalid email",
-            },
-            password:{
-                trim:true,
-                isEmpty:{
-                    negated:true,
-                    errorMessage:"password field empty",
-                    bail:true,
-                },
-                isString:true,
-                isLength:{
-                    errorMessage:"password must be atleast 5 characters long",
-                    options:{min:5},
-                    bail:true,
-                },
-            }
-        }),
-
-        (req,res)=>{
-
-
-            console.log(validationResult(req))
-
-            config.mongo.mongoose_models.user.create({
-                email:"plonk",
-                password:"password",
-                authKey:"auth_testt"
-            })
-            .then((result)=>{
-                console.log("success")
-
-                console.log(result)
-
-                res.status(200).send({
-
-                    ee:result,
-                    result:true,
-                    message:"success?"
-                })
-
-
-            })
-            .catch((err)=>{
-
-                // console.log(err)
-
-                console.log("user field creation error")
-
-                res.status(200).send({
+            const req_errors = validationResult(req).errors
+            if(req_errors.length !== 0){
+                return res.status(200).send({
                     result:false,
-                    message:"error",
-                    error:err
+                    message:req_errors[0].msg
+                })
+            }
+
+                
+                gen_authKey()
+                .then((genned_authKey)=>{
+                    
+
+                    config.mongo.mongoose_models.user.findOne({
+                        email:req.body.email
+                    })
+                    .then((found_result)=>{
+
+                        if(found_result === null){
+                            create_user(req.body.email,req.body.password,genned_authKey)
+                            .then(()=>{
+
+
+                                req.session.authKey = genned_authKey
+                                return res.status(200).send({
+                                    result:true,
+                                    message:"successfully registered.",
+                                    next:undefined,
+                                })
+
+                            })
+                        }
+
+                        if(found_result !== null){
+                            return res.status(200).send({
+                                result:false,
+                                message:"that email is already in use."
+                            })
+                        }
+
+
+                    })
+                    .catch((error)=>{
+                        throw error
+                    })
+
+
 
                 })
+                .catch((error)=>{
+                    next(error)
+                })
 
-            })
+
+
         }
     )
 
 
     return auth_signup_router
 }
+
+
+
+
 
 module.exports = build_auth_signup_router
