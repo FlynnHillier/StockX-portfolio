5b8693e562ebd3553bad6065d91437a3211e3ac7
FlynnHillier <flynnhillier@gmail.com>
2022-03-04 21:59:19 +0000

    configured api/private/ to demand AuthKey presence within session

--- server/Routes/api/private/private_api_RouteHandling.js
+++ server/Routes/api/private/private_api_RouteHandling.js
@@ -5,9 +5,27 @@ function build_private_api_router(mongoose_instance,config){
 
     const private_api_routeHandler = express.Router()
 
+    private_api_routeHandler.use((req,res,next)=>{
+        if(req.session.authKey === undefined){
+            next({
+                expected:true,
+                message:"you do not have access to this resource"
+            })
+        } else{
+            next()
+        }
+
+
+    })
+
+
     private_api_routeHandler.get("/test",(req,res)=>{
         res.status(200).send("Testing private API")
     })
+
+
+
+
 
 
 
