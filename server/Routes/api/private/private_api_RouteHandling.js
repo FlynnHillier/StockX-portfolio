97112fc889262aaa23cb9c2cddf32b4c4ec44eb3
FlynnHillier <flynnhillier@gmail.com>
2022-03-05 09:40:30 +0000

    added api/private/getEmail

--- server/Routes/api/private/private_api_RouteHandling.js
+++ server/Routes/api/private/private_api_RouteHandling.js
@@ -3,25 +3,29 @@ const express = require("express")
 
 function build_private_api_router(mongoose_instance,config){
 
+
+    const getEmail = require(path.join(__dirname,"content","api_private_getEmail"))(mongoose_instance,config)
+
+
     const private_api_routeHandler = express.Router()
 
     private_api_routeHandler.use((req,res,next)=>{
         if(req.session.authKey === undefined){
             next({
                 expected:true,
+                reason_code:"access_denied",
                 message:"you do not have access to this resource"
             })
         } else{
             next()
         }
-
-
     })
 
 
     private_api_routeHandler.get("/test",(req,res)=>{
         res.status(200).send("Testing private API")
     })
+    private_api_routeHandler.use("/getEmail",getEmail)
 
 
 
