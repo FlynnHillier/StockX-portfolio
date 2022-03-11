556e1e1ec8676577f5b39c25da213fd8e9b78fca
FlynnHillier <flynnhillier@gmail.com>
2022-03-11 17:03:30 +0000

    changes to user Schema to support storing of user's 'stock' data

--- server/misc/mongoose_models.js
+++ server/misc/mongoose_models.js
@@ -1,8 +1,99 @@
 
 const mongoose = require("mongoose")
+require('mongoose-type-url')
+
 
 const model = mongoose.model
 const schema = mongoose.Schema
+
+
+
+const sizes_schema = new schema({
+    _id:false,
+    size:{
+        required:true,
+        type:Number,
+        min:0,
+        max:20,
+        validate:{
+            validator: function(number){
+                return number % 0.5 === 0
+            },
+            message:"Not a valid size (must be multiple of 0.5)"
+        }
+    },
+    quantity:{
+        type:Number,
+        min:0,
+        default:0,
+        required:true,
+    }
+
+
+})
+
+
+
+const shoe_schema = new schema({
+    urlKey:{
+        type:String,
+        required:true,
+        default:"__URL KEY__"
+    },
+    sizes:{
+        type:[sizes_schema],
+        required:true,
+        default:[]
+    },
+    title:{
+        type:String,
+        required:true,
+        default:"__SHOE TITLE__"
+    },
+    imageURL:{
+        type:String,
+        required:true,
+        default:"https://www.google.com"
+    },
+    color:{
+        type:String,
+        required:true,
+        default:"__COLOR__",
+    }
+
+
+
+})
+
+
+
+
+
+const stock_schema = new schema({
+    stats:{
+        totalValue:{
+            type:Number,
+            required:true,
+            default:0,
+        },
+        totalItems:{
+            type:Number,
+            required:true,
+            default:0,
+        }
+    },
+    current:{
+        required:true,
+        type:[shoe_schema],
+    },
+    sold:{
+        required:true,
+        type:[shoe_schema]
+    },
+    _id:false,
+})
+
+
 
 mongoose_models = {
 
@@ -20,8 +111,11 @@ mongoose_models = {
             type:String,
             required:true,
             unique:true,
-        }
+        },
+        stock:stock_schema
     })),
+
+
 
 }
 
